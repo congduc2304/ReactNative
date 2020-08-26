403e97a99a53d95715735e7bf108bfd7f8f5fa73
congduc2304 <congduc2304@gmail.com>
2020-08-26 15:59:10 +0700

    bai tap 1

--- App.js
+++ App.js
@@ -1,85 +1,85 @@
-/* eslint-disable react-native/no-inline-styles */
 //BAI1
-// import React, {Component} from 'react';
-// import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+//eslint-disable react-native/no-inline-styles
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
 
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <SafeAreaView
-//         style={{
-//           flex: 1,
-//           flexDirection: 'column',
-//         }}>
-//         <View style={{flex: 1, backgroundColor: 'green'}}>
-//           <View
-//             style={{
-//               flex: 1,
-//               justifyContent: 'center',
-//               alignItems: 'center',
-//               backgroundColor: 'red',
-//             }}>
-//             <Text>1</Text>
-//           </View>
-//         </View>
-//         <View
-//           style={{
-//             flex: 2,
-//             flexDirection: 'row',
-//           }}>
-//           <View
-//             style={{
-//               flex: 1,
-//               justifyContent: 'center',
-//               alignItems: 'center',
-//               backgroundColor: 'yellow',
-//             }}>
-//             <Text>2</Text>
-//           </View>
-//           <View
-//             style={{
-//               flex: 2,
-//               flexDirection: 'column',
-//             }}>
-//             <View
-//               style={{
-//                 flex: 1,
-//                 justifyContent: 'center',
-//                 alignItems: 'center',
-//                 backgroundColor: 'blue',
-//               }}>
-//               <Text>3</Text>
-//             </View>
-//             <View
-//               style={{
-//                 flex: 1,
-//                 flexDirection: 'row',
-//               }}>
-//               <View
-//                 style={{
-//                   flex: 1,
-//                   justifyContent: 'center',
-//                   alignItems: 'center',
-//                   backgroundColor: 'pink',
-//                 }}>
-//                 <Text>4</Text>
-//               </View>
-//               <View
-//                 style={{
-//                   flex: 1,
-//                   justifyContent: 'center',
-//                   alignItems: 'center',
-//                   backgroundColor: 'purple',
-//                 }}>
-//                 <Text>5</Text>
-//               </View>
-//             </View>
-//           </View>
-//         </View>
-//       </SafeAreaView>
-//     );
-//   }
-// }
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+        }}>
+        <View style={{flex: 1, backgroundColor: 'green'}}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'red',
+            }}>
+            <Text>1</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 2,
+            flexDirection: 'row',
+          }}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'yellow',
+            }}>
+            <Text>2</Text>
+          </View>
+          <View
+            style={{
+              flex: 2,
+              flexDirection: 'column',
+            }}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'blue',
+              }}>
+              <Text>3</Text>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'pink',
+                }}>
+                <Text>4</Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'purple',
+                }}>
+                <Text>5</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 //BAI2
 // import React, {Component} from 'react';
@@ -172,121 +172,121 @@
 // }
 
 //BAI3
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+// import React, {Component} from 'react';
+// import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
 
-export default class BaiTap3 extends Component {
-  render() {
-    return (
-      <SafeAreaView
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-        }}>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'green',
-            }}>
-            <Text>1</Text>
-          </View>
-          <View
-            style={{
-              flex: 2,
-              flexDirection: 'column',
-            }}>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'red',
-              }}>
-              <Text>3</Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'bisque',
-              }}>
-              <Text>4</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'yellow',
-            }}>
-            <Text>2</Text>
-          </View>
-          <View
-            style={{
-              flex: 2,
-              flexDirection: 'column',
-            }}>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'blue',
-              }}>
-              <Text>5</Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'purple',
-              }}>
-              <Text>6</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: 'column',
-            }}>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'brown',
-              }}>
-              <Text>7</Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'darkgoldenrod',
-              }}>
-              <Text>8</Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'gray',
-              }}>
-              <Text>9</Text>
-            </View>
-          </View>
-        </View>
-      </SafeAreaView>
-    );
-  }
-}
+// export default class BaiTap3 extends Component {
+//   render() {
+//     return (
+//       <SafeAreaView
+//         style={{
+//           flex: 1,
+//           flexDirection: 'column',
+//         }}>
+//         <View style={{flex: 1, flexDirection: 'row'}}>
+//           <View
+//             style={{
+//               flex: 1,
+//               justifyContent: 'center',
+//               alignItems: 'center',
+//               backgroundColor: 'green',
+//             }}>
+//             <Text>1</Text>
+//           </View>
+//           <View
+//             style={{
+//               flex: 2,
+//               flexDirection: 'column',
+//             }}>
+//             <View
+//               style={{
+//                 flex: 1,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 backgroundColor: 'red',
+//               }}>
+//               <Text>3</Text>
+//             </View>
+//             <View
+//               style={{
+//                 flex: 1,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 backgroundColor: 'bisque',
+//               }}>
+//               <Text>4</Text>
+//             </View>
+//           </View>
+//         </View>
+//         <View style={{flex: 1, flexDirection: 'row'}}>
+//           <View
+//             style={{
+//               flex: 1,
+//               justifyContent: 'center',
+//               alignItems: 'center',
+//               backgroundColor: 'yellow',
+//             }}>
+//             <Text>2</Text>
+//           </View>
+//           <View
+//             style={{
+//               flex: 2,
+//               flexDirection: 'column',
+//             }}>
+//             <View
+//               style={{
+//                 flex: 1,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 backgroundColor: 'blue',
+//               }}>
+//               <Text>5</Text>
+//             </View>
+//             <View
+//               style={{
+//                 flex: 1,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 backgroundColor: 'purple',
+//               }}>
+//               <Text>6</Text>
+//             </View>
+//           </View>
+//           <View
+//             style={{
+//               flex: 1,
+//               flexDirection: 'column',
+//             }}>
+//             <View
+//               style={{
+//                 flex: 1,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 backgroundColor: 'brown',
+//               }}>
+//               <Text>7</Text>
+//             </View>
+//             <View
+//               style={{
+//                 flex: 1,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 backgroundColor: 'darkgoldenrod',
+//               }}>
+//               <Text>8</Text>
+//             </View>
+//             <View
+//               style={{
+//                 flex: 1,
+//                 justifyContent: 'center',
+//                 alignItems: 'center',
+//                 backgroundColor: 'gray',
+//               }}>
+//               <Text>9</Text>
+//             </View>
+//           </View>
+//         </View>
+//       </SafeAreaView>
+//     );
+//   }
+// }
